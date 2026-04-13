1a5fd72338fcdf3561f5c266629e5a55b969749f
Leonardo <rmalonbazan@gmail.com>
2026-04-13 00:38:28 -0500

    feat(query-understanding): add LLM service to parse natural language into structured query

---
+++ backend/services/understanding.py
@@ -0,0 +1,28 @@
+"""
+LLM wrapper for returning a structured query.
+
+"""
+
+#Local modules
+from rag.llm.ollama import ChatOllama
+from rag.llm.prompt_templates import build_query
+
+
+class QueryUnderstandingService:
+    def __init__(self, api_key:str):
+        self.api_key = api_key
+    
+    def parse(self, query: str) -> dict:
+        # call LLM
+        llm = ChatOllama(self.api_key)
+
+        ALLOWED_MOODS = ["romántico","energético","relajado","misterioso","divertido","cultural",
+            "artistico","nocturno","familiar","intenso","fiesta","educativo","fiestero","espontáneo",
+            "elegante","underground","deportivo","gastronómico","urbano","desconexión","aire-libre",
+            "natural","aventurero","foodie","casual","buen-ambiente","extremo"]
+
+        prompt = build_query(query,ALLOWED_MOODS)
+
+        structured_query = llm.invoke(prompt)
+
+        return structured_query
