97f328db15121cffc9cb00ad7bc4d9aefdee9b29
Leonardo <rmalonbazan@gmail.com>
2026-04-13 00:40:15 -0500

    feat(vectordb): add configuration for vector database integration

---
+++ rag/retrieval/query_pipeline.py
@@ -0,0 +1,32 @@
+"""
+Retrieval pipeline script
+
+"""
+
+#Standard module
+from pathlib import Path
+
+#Langchain module
+
+#Local modules
+from ..ingestion.embeddings import get_huggingface_embedding
+from ..vectordb.chroma_db import Persistent_ChromaDB
+
+# ==========================================
+# 1. INITIAL CONFIGURATION (PATHS AND VARIABLES)
+# ==========================================
+
+BASE_DIR = Path(__file__).parent.parent.parent
+
+persistent_db_path =  BASE_DIR/"data"/ "chroma_db"
+
+#Embedding model (HuggingFace)
+embedding_function = get_huggingface_embedding()
+
+# ==========================================
+# 2. VECTOR DB SETUP
+# ==========================================
+
+#---SEARCH AND RANK RELEVANT DOCS---
+vector_db = Persistent_ChromaDB(persistent_db_path,get_huggingface_embedding())
+
