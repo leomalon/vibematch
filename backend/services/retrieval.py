8cbf4ffe7d655f483b0a467eaa26d10aa0a81aac
Leonardo <rmalonbazan@gmail.com>
2026-04-13 00:28:52 -0500

    fix(retrieval-service): change response type to string to prevent prompt injection issues

--- backend/services/retrieval.py
+++ backend/services/retrieval.py
@@ -8,28 +8,28 @@ class RetrievalService:
     def __init__(self, vectorstore):
         self.vectorstore = vectorstore
 
-    def search_top_n(self, query: str, collection_name:str, k: int = 5,top_n:int=7):
+    def search_top_n(self, query: str, collection_name:str, k: int = 7,top_n:int=5):
         results = self.vectorstore.search_documents(query,collection_name,k)
 
-        docs = results["documents"][0]
-        docs_metadata = results["metadatas"][0]
+        events = results["documents"][0]
+        events_metadata = results["metadatas"][0]
         distances = results["distances"][0]
 
         # Pair and sort (lower distance = better)
-        ranked_documents = sorted(zip(docs, distances,docs_metadata), key=lambda x: x[1])
+        ranked_events = sorted(zip(events, distances,events_metadata), key=lambda x: x[1])
 
         # Keep top-n only
-        top_docs = [(doc,metadata) for doc, _,metadata in ranked_documents[:top_n]]
+        top_events = [metadata for event, _,metadata in ranked_events[:top_n]]
 
 
-        events = [
-            f"[EVENTO {i+1}]\n"
-            f"{doc}\n"
-            f"Título: {meta.get('titulo')}\n"
-            f"URL: {meta.get('url')}"
-            f"Categoria evento: {meta.get("categoria")}"
-            f"Precio: {meta.get("moneda")} {meta.get("precio")}"
-            for i, (doc, meta) in enumerate(top_docs)
-        ]
+        # events = [
+        #     f"[EVENTO {i+1}]\n"
+        #     f"{doc}\n"
+        #     f"Título: {meta.get('titulo')}\n"
+        #     f"URL: {meta.get('url')}"
+        #     f"Categoria evento: {meta.get("categoria")}"
+        #     f"Precio: {meta.get("moneda")} {meta.get("precio")}"
+        #     for i, (doc, meta) in enumerate(top_docs)
+        # ]
 
-        return events
+        return str(top_events)
