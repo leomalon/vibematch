c9c41e5a93b1b68b393d201299ec4604218f99df
Leonardo <rmalonbazan@gmail.com>
2026-04-13 00:19:57 -0500

    feat(cors): enable frontend access via CORS configuration

--- backend/main.py
+++ backend/main.py
@@ -6,8 +6,18 @@ Initializes FastAPI app and registers routes
 """
 
 from fastapi import FastAPI
-from routes import events
+from .routes import events
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # for dev only
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(events.router, prefix="/events")
