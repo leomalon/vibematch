dbb7be557892d333889c237f0d89d9c9d54959d7
Leonardo <rmalonbazan@gmail.com>
2026-04-22 23:31:11 -0500

    refactor(frontend): replace hardcoded API URL with environment variable

--- frontend/vibe_match/src/app/page.js
+++ frontend/vibe_match/src/app/page.js
@@ -24,7 +24,7 @@ export default function Home() {
     setEvents([]);
 
     try {
-      const res = await fetch("http://127.0.0.1:8000/events/search", {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/events/search`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
