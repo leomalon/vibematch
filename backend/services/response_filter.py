d0c16584e2279abc0f19eae9a314da9ac0632b58
Leonardo <rmalonbazan@gmail.com>
2026-05-01 17:23:12 -0500

    feat(backend-response-filter): remove ollama client to allow the use of different clients

--- backend/services/response_filter.py
+++ backend/services/response_filter.py
@@ -6,16 +6,15 @@ LLM wrapper for response filter.
 #Standard module
 import json
 
-from rag.llm.ollama import ChatOllama
 from rag.llm.prompt_templates import build_recommendation_query
 
 class ResponseFilterService:
 
-    def __init__(self, api_key):
-        self.api_key= api_key
+    def __init__(self, llm_client):
+        self.llm_client = llm_client
 
     def json_response(self,context:str,query:str):
-        llm = ChatOllama(self.api_key)
+        llm = self.llm_client
         prompt = build_recommendation_query(context,query)
         response = llm.invoke(prompt)
 
