afd137b661e7b4385e73c0ce1928e5d6679fc913
Leonardo <rmalonbazan@gmail.com>
2026-05-01 18:07:18 -0500

    feat(query-pipeline): change ollama embedding function to openai embedding function

--- rag/retrieval/query_pipeline.py
+++ rag/retrieval/query_pipeline.py
@@ -6,10 +6,11 @@ Retrieval pipeline script
 #Standard module
 from pathlib import Path
 
-#Langchain module
+#Third-party modules
+from dotenv import load_dotenv
 
 #Local modules
-from rag.ingestion.embeddings import get_huggingface_embedding
+from rag.ingestion.embeddings import get_openai_embedding
 from rag.vectordb.chroma_db import Persistent_ChromaDB
 
 # ==========================================
@@ -20,13 +21,14 @@ BASE_DIR = Path(__file__).parent.parent.parent
 
 persistent_db_path =  BASE_DIR/"data"/ "chroma_db"
 
-#Embedding model (HuggingFace)
-embedding_function = get_huggingface_embedding()
+#Embedding model
+load_dotenv()
+embedding_function = get_openai_embedding(model_name = "text-embedding-3-large")
 
 # ==========================================
 # 2. VECTOR DB SETUP
 # ==========================================
 
 #---SEARCH AND RANK RELEVANT DOCS---
-vector_db = Persistent_ChromaDB(persistent_db_path,get_huggingface_embedding())
+vector_db = Persistent_ChromaDB(persistent_db_path,embedding_function)
 
